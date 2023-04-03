a3968211921780c721bf24b99a283ae66fd21014
leosg <leopold.sallan-gemard@epitech.eu>
2023-04-03 19:07:25 +0200

    [ADD] Doxygen documentation for interface method

--- Interface/Game/IGame.hpp
+++ Interface/Game/IGame.hpp
@@ -8,28 +8,45 @@
 #pragma once
 #include "../Graphical/IGraphicalFactory.hpp"
 
-class IGame {
+class IGame
+{
     public:
         ~IGame() = default;
 
         /**
-         * @brief Get the Event Binding object
-         *
+         * @brief Returns the event handler object for the window.
+         * Its purpose is to return the event handler object for the window, allowing the client to
+         * modify or access event bindings.
+         * The function returns a reference to the IWindow::EventHandler object associated with the
+         * window.
          * @return IWindow::EventHandler&
          */
         virtual IWindow::EventHandler &getEventBinding() = 0;
 
         /**
-         * @brief
-         *
+         * @brief Process a single tick of the game or simulation.
+         * Its purpose is to process a single tick of the game or simulation, updating the state of
+         * the game or simulation accordingly. The function takes four parameters: a reference to an
+         * IGrid object representing the game board or simulation space, a reference to an IText
+         * object representing the score or other information to be displayed, a reference to an
+         * IClock object representing the game or simulation timer, and a reference to an IText
+         * object representing the time or other information to be displayed. The function returns a
+         * boolean value indicating whether the game or simulation should continue running. If
+         * false, the game or simulation should end and any necessary cleanup should be performed.
+         * @param grid
+         * @param scoreText
+         * @param timeText
+         * @param clock
          * @return true
          * @return false
          */
-        virtual bool processGameTick(IGrid &, IText &, IText &, IClock &) = 0;
+        virtual bool processGameTick(IGrid &grid, IText &scoreText, IText &timeText,
+                                     IClock &clock) = 0;
 
         /**
-         * @brief
-         *
+         * @brief Restarts the implementation's game or simulation.
+         * Its purpose is to restart the game or simulation implemented by the class, resetting all
+         * necessary variables and objects to their initial state.
          */
         virtual void restart() = 0;
 };
--- Interface/Graphical/IEntity.hpp
+++ Interface/Graphical/IEntity.hpp
@@ -8,92 +8,104 @@
 #pragma once
 #include <string>
 
-class IEntity {
-public:
-    enum class Color {
-        Blue,
-        Green,
-        Orange,
-        Red,
-        Brown,
-        Yellow,
-    };
+class IEntity
+{
+    public:
+        enum class Color {
+            Blue,
+            Green,
+            Orange,
+            Red,
+            Brown,
+            Yellow,
+        };
 
-    ~IEntity() = default;
+        ~IEntity() = default;
 
-    /**
-     * @brief Set the Position object
-     *
-     * @param x
-     * @param y
-     */
-    virtual void setPosition(int x, int y) = 0;
+        /**
+         * @brief Sets the position of the entity.
+         * Its purpose is to set the position of the entity to the specified x and y coordinates.
+         * The function takes two integer parameters representing the x and y coordinates to set as
+         * the new position of the entity.
+         * @param x
+         * @param y
+         */
+        virtual void setPosition(int x, int y) = 0;
 
-    /**
-     * @brief
-     *
-     */
-    virtual void destroy() = 0;
+        /**
+         * @brief  Destroys any resources used in the implementation.
+         * Its purpose is to destroy any resources used in the implementation, such as windows or
+         * textures.
+         */
+        virtual void destroy() = 0;
 
-    /**
-     * @brief
-     *
-     */
-    virtual void displayEntity() = 0;
+        /**
+         * @brief Displays an entity on the screen.
+         * Its purpose is to display an entity on the screen, such as a sprite or image.
+         */
+        virtual void displayEntity() = 0;
 };
 
-class IText
-    : public IEntity
+class IText : public IEntity
 {
-public:
-    enum class EntityType {
-        GridEntity,
-        SpriteEntity,
-        TextEntity,
-    };
+    public:
+        enum class EntityType {
+            GridEntity,
+            SpriteEntity,
+            TextEntity,
+        };
+        ~IText() = default;
 
-    ~IText() = default;
+        /**
+         * @brief Creates a new object in the implementation.
+         * Its purpose is to create a new object in the implementation based on the given string
+         * parameter.
+         * The function take a string as parameter, which can be used to determine the
+         * type or properties of the object to be created
+         * @param str
+         */
+        virtual void create(std::string str) = 0;
 
-    /**
-     * @brief
-     *
-     * @param str
-     */
-    virtual void create(std::string str) = 0;
-
-    /**
-     * @brief
-     *
-     * @param str
-     */
-    virtual void changeString(std::string str) = 0;
+        /**
+         * @brief Changes the string property of the object in the implementation.
+         * Its purpose is to change the string property of the object in the implementation.
+         * The function takes a string parameter, which represents the new value for the
+         * string property of the object.
+         * @param str
+         */
+        virtual void changeString(std::string str) = 0;
 };
 
-class IGrid
-    : public IEntity
+class IGrid : public IEntity
 {
-public:
-    struct Cell {
-        Color color;
-        size_t x;
-        size_t y;
-    };
+    public:
+        struct Cell {
+                Color color;
+                size_t x;
+                size_t y;
+        };
+        ~IGrid() = default;
 
-    ~IGrid() = default;
+        /**
+         * @brief Creates a new object in the implementation with the given width and height.
+         * Its purpose is to create a new object in the implementation with the given width and
+         * height.
+         * The function takes two integer parameters, representing the width and height of the new
+         * object to be created.
+         * @param width
+         * @param heigth
+         */
+        virtual void create(int width, int heigth) = 0;
 
-    /**
-     * @brief
-     *
-     * @param width
-     * @param heigth
-     */
-    virtual void create(int width, int heigth) = 0;
-
-    /**
-     * @brief
-     *
-     * @param x
-     * @param y
-     */
-    virtual void updateCell(int x, int y, IEntity::Color) = 0;
+        /**
+         * @brief a pure virtual function that must be implemented by the derived class.
+         * Its purpose is to update the color of a cell in the implementation's grid, identified by
+         * its x and y coordinates.
+         * The function takes three parameters: two integer parameters representing the x and y
+         * coordinates of the cell, and a parameter of type IEntity::Color representing the new
+         * color to assign to the cell.
+         * @param x
+         * @param y
+         */
+        virtual void updateCell(int x, int y, IEntity::Color) = 0;
 };
