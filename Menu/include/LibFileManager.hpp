d609917a0f914724f3310bc76c0460474cc6b085
raphael <camblong.raphael@epitech.eu>
2023-04-04 11:43:25 +0200

    [ADD] menu file management and selector

---
+++ Menu/include/LibFileManager.hpp
@@ -0,0 +1,60 @@
+/*
+** EPITECH PROJECT, 2023
+** arcade
+** File description:
+** LibFileManager
+*/
+
+#pragma once
+#include <string>
+#include <vector>
+
+namespace Lib
+{
+    enum libType{
+        _GRAPHICALS_,
+        _GAMES_,
+        _MENU_,
+    };
+
+    struct lib {
+        std::string name;
+        std::string path;
+        libType type;
+
+        bool operator==(const lib& other) const {
+           return type == other.type && name == other.name && path == other.path;
+        }
+    };
+};
+
+class LibFileManager {
+    private:
+        Lib::libType getLibType(const std::string &);
+        std::string _pathLib;
+        std::vector<Lib::lib> _availableLibs;
+
+        std::vector<Lib::lib> GraphicalStack;
+
+        std::vector<Lib::lib> GameStack;
+
+    public:
+        LibFileManager(const std::string &path);
+        ~LibFileManager();
+        Lib::lib ghraphicalCurrent;
+        Lib::lib gameCurrent;
+
+        void refreshAvailableLib();
+        std::vector<Lib::lib> availableLib();
+        bool isAvailable(const std::string &);
+        void displayavailableLib();
+        Lib::lib getLibByName(const std::string &name);
+        Lib::lib getLibBypath(const std::string &name);
+
+        Lib::lib getNextGraphicalsLib(const Lib::lib &loaded);
+        Lib::lib getNextGameLib(const Lib::lib &loaded);
+
+        std::vector<Lib::lib> getGameLibs();
+        std::vector<Lib::lib> getGraphicsLibs();
+};
+
--- Menu/include/Menu.hpp
+++ Menu/include/Menu.hpp
@@ -6,14 +6,29 @@
 */
 
 #pragma once
+#include "LibFileManager.hpp"
 #include "../../Interface/Menu/IMenu.hpp"
 #include "../../Interface/Graphical/IGraphicalFactory.hpp"
 
 class Menu: public IMenu
 {
     private:
-        IWindow::EventHandler gameEvent;
+        // file management:
+        LibFileManager _libFileManager;
+        std::vector<Lib::lib> _gameLibs;
+        std::vector<Lib::lib> _graphicLibs;
+        size_t _gameLibsSelected;
+        size_t _graphicsLibsSelected;
 
+        // render
+        std::string getLibGameName(size_t);
+        std::string getLibGraphicsName(size_t);
+        void displayTexts(IText &, IText &, std::array<std::reference_wrapper<IText>, 3>, std::array<std::reference_wrapper<IText>, 3>);
+
+        //event
+        LambdaLoadingGraphicsByName _loadGraphics;
+        LambdaLoadingGameByName _LoadGame;
+        IWindow::EventHandler _gameEvent;
         void upKeyPress();
         void downKeyPress();
         void leftKeyPress();
